9dad8ca4ed24b76a40fd336a7ba105f58520fed6
beecodeguy <beecodeguy@gmail.com>
2025-04-03 15:49:47 +0545

    Updates

--- contexts/StyleContext.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const StyleContext = React.createContext();
-export const StyleProvider = StyleContext.Provider;
-export const StyleConsumer = StyleContext.Consumer;
-export default StyleContext;
-//# sourceMappingURL=StyleContext.js.map
