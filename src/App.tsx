3f42da91b5e5b381b6729c0742fa95878b02716f
beecodeguy <beecodeguy@gmail.com>
2025-04-19 11:27:05 +0545

    updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Main from "./containers/Main";
 import "./App.scss";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import MyStoryScreen from "./containers/my-story/my-story.screen";
 import NavLayout from "./containers/Layouts/NavLayout";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<NavLayout />}>
           <Route path="/" element={<Main />} />
           <Route path="/my-story" element={<MyStoryScreen />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
