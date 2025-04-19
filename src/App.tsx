b1e56024efc0430dba8eaa601b9e0b9ce44ab231
beecodeguy <beecodeguy@gmail.com>
2025-04-19 11:37:56 +0545

    updated router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
 import React from "react";
 import Main from "./containers/Main";
 import "./App.scss";
-import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  createHashRouter,
+  HashRouter,
+  Outlet,
+  Route,
+  RouterProvider,
+  Routes
+} from "react-router-dom";
 import MyStoryScreen from "./containers/my-story/my-story.screen";
 import NavLayout from "./containers/Layouts/NavLayout";
 
 function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<NavLayout />}>
-          <Route path="/" element={<Main />} />
-          <Route path="/my-story" element={<MyStoryScreen />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <NavLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Main />
+        },
+        {
+          path: "/my-story",
+          element: <MyStoryScreen />
+        }
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
+
+  // return (
+  //   <HashRouter>
+  //     <Routes>
+  //       <Route path="/" element={<NavLayout />}>
+  //         <Route path="/" element={<Main />} />
+  //         <Route path="/my-story" element={<MyStoryScreen />} />
+  //       </Route>
+  //     </Routes>
+  //   </HashRouter>
+  // );
 }
 
 export default App;
