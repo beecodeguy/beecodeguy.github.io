3344285950395734cf336aefee6bf6a0c88aab91
beecodeguy <beecodeguy@gmail.com>
2025-04-19 12:27:56 +0545

    link to my story updated

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -29,6 +29,20 @@ const Main = () => {
       {/* <Talks /> */}
       {/* <Twitter /> */}
       {/* <Podcast /> */}
+      <div
+        className="badge-base LI-profile-badge"
+        data-locale="en_US"
+        data-size="medium"
+        data-theme="dark"
+        data-type="VERTICAL"
+        data-vanity="np-beezay"
+        data-version="v1"
+      >
+        <a
+          className="badge-base__link LI-simple-link"
+          href="https://np.linkedin.com/in/np-beezay?trk=profile-badge"
+        ></a>
+      </div>
     </>
   );
 };
